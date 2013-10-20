365aa2823357f054f2d6a9437b772a47abbb79a5
Davidi <Davidi@192.168.2.2>
2013-10-20 15:59:47 -0400

    Starting it up

---
+++ src/data/Match.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author Davidi
+ */
+public class Match {
+    private String[] players;
+    private String winner;  //Do not initialize-- this is a pointer.
+    private int winnerID;
+    
+    public Match()
+    {
+    }
+}
